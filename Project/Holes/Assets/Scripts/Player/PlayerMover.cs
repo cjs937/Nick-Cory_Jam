cd772b6b8bf1c345d938d89d4b482ddd9b4ffa32
MSI\Cory <smithcory937@gmail.com>
2018-01-10 12:07:19 -0500

    Add player movement and gitignore. Removed some unneeded files

---
+++ Project/Holes/Assets/Scripts/Player/PlayerMover.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    //Speed when player isn't carrying any memes
+    public float baseSpeed;
+
+    //Clamps player to screen
+    public Vector2[] clampPositions = new Vector2[2] { Vector2.zero, Vector2.zero };
+
+    //% cut in speed when player cathces meme
+    public float speedDecreasePercent;
+
+    float currentSpeed;
+    Rigidbody2D physics = null;
+
+    private void OnDrawGizmosSelected()
+    {
+        Vector3 cubeSize = new Vector3(1, 1, 1);
+
+        Gizmos.DrawCube((Vector3)clampPositions[0], cubeSize);
+
+        Gizmos.DrawCube((Vector3)clampPositions[1], cubeSize);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        physics = GetComponent<Rigidbody2D>();
+        currentSpeed = baseSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        lerpToMouse();	
+	}
+
+    void lerpToMouse()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+
+        float newX = transform.position.x;
+
+        newX = Mathf.Lerp(newX, mousePos.x, currentSpeed * Time.deltaTime * Time.deltaTime);
+
+        newX = Mathf.Clamp(newX, clampPositions[0].x, clampPositions[1].x);
+
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
+}
---
+++ Project/Holes/Assets/Scripts/Utility/Utility.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utility : MonoBehaviour
+{
+    public static float DEFAULT_GRAVITY
+    {
+        get { return -25.0f; }
+        private set { }
+    }
+        
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static float calcJumpForce(float _height, float _length)
+    {
+        return ((2 * _height) / _length);
+    }
+
+    public static float calcJumpGravity(float _height, float _length)
+    {
+        return (-2 * _height) / (_length * _length);
+    }
+
+    public static Vector2 getMousePosition()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    public static Vector2 getScreenMin(Camera _camera = null)
+    {
+        Camera screen;
+
+        if (_camera == null)
+            screen = Camera.main;
+        else
+            screen = _camera;
+
+        return screen.ScreenToWorldPoint(Vector2.zero);
+    }
+
+    public static Vector2 getScreenMax(Camera _camera = null)
+    {
+        Camera screen;
+
+        if (_camera == null)
+            screen = Camera.main;
+        else
+            screen = _camera;
+
+        return screen.ScreenToWorldPoint(new Vector2(screen.pixelWidth, screen.pixelHeight));
+    }
+
+    public static Vector2 convertToCameraSpace(Camera _camera, Vector2 _position)
+    {
+        float x = _camera.transform.position.x + _position.x; //+ (_camera.transform.position.x - _position.x);
+
+        float y = _camera.transform.position.y + _position.y; //+ (_camera.transform.position.y - _position.y);
+
+        return new Vector2(x, y);
+    }
+
+    public static bool isOffScreen(Vector2 _position)
+    {
+        Vector2 screenMin = getScreenMin();
+        Vector2 screenMax = getScreenMax();
+
+        if (_position.x < screenMin.x || screenMax.x < _position.x)
+        {
+            return true;
+        }
+
+        if (_position.y < screenMin.y || screenMax.y < _position.y)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static Vector2 mutliplyVectors(Vector2 _vectorA, Vector2 _vectorB)
+    {
+        return new Vector2(_vectorA.x * _vectorB.x, _vectorA.y * _vectorB.y);
+    }
+}
+
+public struct Tuple3<A, B, C>
+{
+
+    public A first
+    {
+        get;
+        private set;
+    }
+
+    public B second
+    {
+        get;
+        private set;
+    }
+
+    public C third
+    {
+        get;
+        private set;
+    }
+
+    public Tuple3(A first, B second, C third)
+    {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+}

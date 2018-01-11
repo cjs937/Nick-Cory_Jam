6ba8323f2f968b29bd6cb61c7b5eaaa80b2f0228
Nick Magnus <nickjmagnus@gmail.com>
2018-01-11 13:15:08 -0500

    Adding scoring system and ui

---
+++ Project/Holes/Assets/ScoreKeeperScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeperScript : MonoBehaviour {
+
+	int CurrentScore;
+	public int DefaultScoreIncrement = 50;
+
+	public Text ScoreText;
+
+	void Start () {
+		CurrentScore = 0;
+	}
+	void Update () {
+		ScoreText.text = "Score: " + CurrentScore.ToString();
+	}
+
+	public void IncrementScore(int _increment)
+	{
+		CurrentScore += _increment;
+	}
+	public void IncrementScore()
+	{
+		CurrentScore += DefaultScoreIncrement;
+	}
+}

e731d54b4aef36ffee7b66f0d79bc28cde504aca
MSI\Cory <smithcory937@gmail.com>
2018-01-10 19:02:50 -0500

    Add throwing away memes

--- Project/Holes/Assets/Scripts/Player/MemeCatcher.cs
+++ Project/Holes/Assets/Scripts/Player/MemeCatcher.cs
@@ -7,6 +7,7 @@ public class MemeCatcher : MonoBehaviour
     //position where caught memes will go
     public Transform carryPosition;
 
+    Stack<Meme> heldMemes;
     BoxCollider2D hitBox;
 
     //Distance above player that memes increment by
@@ -14,16 +15,12 @@ public class MemeCatcher : MonoBehaviour
 
     private void Start()
     {
+        heldMemes = new Stack<Meme>();
+
         hitBox = GetComponent<BoxCollider2D>();
 
-        carryPosition.position = new Vector2(transform.position.x, hitBox.bounds.max.y + carryOffset);
+        updateCarryPosition();
     }
-
-    // Update is called once per frame
-    void Update ()
-    {
-		
-	}
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -35,8 +32,24 @@ public class MemeCatcher : MonoBehaviour
         catchMeme(caughtMeme);
     }
 
+    //throw away meme on top
+    public void throwAwayTop()
+    {
+        if (heldMemes.Count == 0)
+            return;
+
+        Destroy(heldMemes.Peek().gameObject);
+
+        heldMemes.Pop();
+
+        updateCarryPosition();
+    }
+
     void catchMeme(Meme _caughtMeme)
     {
+        if (heldMemes.Contains(_caughtMeme))
+            return;
+
         _caughtMeme.onCatch(this);
 
         _caughtMeme.transform.position = carryPosition.position;
@@ -45,7 +58,24 @@ public class MemeCatcher : MonoBehaviour
 
         _caughtMeme.transform.parent = transform;
 
-        //increment carry position
-        carryPosition.position = new Vector2(transform.position.x, carryPosition.position.y + carryOffset);
+        heldMemes.Push(_caughtMeme);
+
+        updateCarryPosition();
+    }
+
+    void updateCarryPosition()
+    {
+        float newCarryY;
+
+        if (heldMemes.Count == 0)
+        {
+            newCarryY = transform.position.y + carryOffset;
+        }
+        else
+        {
+            newCarryY = heldMemes.Peek().transform.position.y + carryOffset;
+        }
+
+        carryPosition.position = new Vector2(transform.position.x, newCarryY);
     }
 }
---
+++ Project/Holes/Assets/Scripts/Trash.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+    public MemeCatcher player;
+    public int trashedCount;
+
+    bool playerInRange = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        trashedCount = 0;	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(playerInRange && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            player.throwAwayTop();
+            trashedCount++;
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!playerInRange && collision.gameObject == player.gameObject)
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (playerInRange && collision.gameObject == player.gameObject)
+        {
+            playerInRange = false;
+        }
+    }
+
+}
